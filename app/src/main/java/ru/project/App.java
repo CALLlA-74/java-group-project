e543cb308b8b75f0ad9e7d16e61da1ebc20a26f5
CALLlA-74 <56507954+CALLlA-74@users.noreply.github.com>
2025-12-27 21:57:41 +0300

    Add building project at file App.java

--- app/src/main/java/ru/project/App.java
+++ app/src/main/java/ru/project/App.java
@@ -1,7 +1,24 @@
 package ru.project;
 
+import java.util.List;
+
+import ru.project.CI.ConsoleInterface;
+import ru.project.Domain.models.Student;
+import ru.project.Lib.List.LightList;
+import ru.project.Repository.IStudentRepo;
+import ru.project.Repository.ListStudRepo.ListStudRepo;
+import ru.project.Service.IStudentService;
+import ru.project.Service.StudService.StudService;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hi!");
+        new App().build();
+    }
+
+    private void build() {
+        IStudentRepo repo = new ListStudRepo((List<Student>)new LightList<Student>());
+        IStudentService service = new StudService(repo);
+        var consoleInterface = new ConsoleInterface(service);
+        consoleInterface.run();
     }
 }
