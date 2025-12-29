549bae131ab2bc2d216cbcf0770efa91a88f72c3
Beyond <BeyondB1rthday@yandex.ru>
2025-12-29 15:52:40 +0500

    Написал тест MultiThreadCounterTest + Mock + build.gradle

---
+++ app/src/test/java/ru/project/Service/StudService/mocks/MockStudRepo.java
@@ -0,0 +1,37 @@
+package ru.project.Service.StudService.mocks;
+
+import java.util.List;
+
+import ru.project.Domain.models.Student;
+import ru.project.Repository.IStudentRepo;
+
+public class MockStudRepo implements IStudentRepo {
+    private List<Student> studs;
+
+    public MockStudRepo(List<Student> studs) {
+        this.studs = studs;
+    }
+
+    @Override
+    public boolean store(Student s) {
+        return studs.add(s);
+    }
+
+    @Override
+    public List<Student> getAll() {
+        return studs;
+    }
+
+    @Override
+    public int searchStud(Student stud) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'searchStud'");
+    }
+
+    @Override
+    public void removeAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeAll'");
+    }
+    
+}
